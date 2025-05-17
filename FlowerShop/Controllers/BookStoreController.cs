6253e7bc8bc6433d7f641dd81187f2a6c8efd92b
wcraw <galaxytech44@outlook.com>
2025-05-17 11:37:10 -0500

    Add project files.

---
+++ FlowerShop/Controllers/BookStoreController.cs
@@ -0,0 +1,96 @@
+﻿using FlowerShop.Models.BookEntityFramework;
+using FlowerShop.Models;
+using System.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlowerShop.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        private BOOKSHOPEntitie database = new BOOKSHOPEntitie();  
+        
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        //selectedAuthor is the value of the author that was selected from the drop down
+        public ActionResult Author(int? selectedAuthor = null)
+        {
+            //authors is used to hold all the values of the authors table into a list
+            var authors = database.AUTHORs.ToList();
+            //Authors is used to get the authors AUTHOR_NUM and AUTHOR_LAST from the authors table
+            ViewBag.Authors = new SelectList(authors, "AUTHOR_NUM", "AUTHOR_LAST", selectedAuthor);
+            //books is used to get the entire book database and filiter out all information with the selected author
+            var books = selectedAuthor.HasValue
+                        ? database.WROTEs.Where(w => w.AUTHOR_NUM == selectedAuthor.Value)
+                                          .Select(w => w.BOOK)
+                                          .ToList()
+                        : new List<BOOK>();  
+            //return view returns all of the information on the authors books
+            return View(books);
+        }
+        //Inventory is the page where you can selected books by clicking on there images
+        public ActionResult Inventory()
+        {
+            //books is used to hold the information of all books within the books database
+            var books = database.BOOKs.Include("INVENTORies").Include("WROTEs").Include("PUBLISHER").ToList();
+            //return is used to return the books database to be used in the webpage
+            return View(books);
+        }
+        //Location is used to find books based off a book store location
+        public ActionResult Location(int? selectedBranch = null)
+        {
+           //branches is used to get the information from the book database BRANCHes table and 
+           //convert that data into a list
+            var branches = database.BRANCHes.ToList();
+            //Branches is used to hold the values of the branches table as a viewbag
+            ViewBag.Branches = branches;
+           //SelectedBranch is used to hold the values of the selectedBranched which is inputed by the user to get 
+           //specific informtion on that store branch
+            ViewBag.SelectedBranch = selectedBranch;
+            //bookds is used to hold all of the data from the book database and sort out information that is from the selected store 
+            //branch
+            var books = selectedBranch.HasValue
+                        ? database.INVENTORies.Where(i => i.BRANCH_NUM == selectedBranch.Value)
+                                               .Select(i => i.BOOK)
+                                               .ToList()
+                        : new List<BOOK>(); 
+            //Books is used to hold the ViewBag value to be used within the Location webpage of the book database information
+            ViewBag.Books = books;
+            return View();
+        }
+        //Publisher is the webpage used to look of books by there publisher
+        public ActionResult Publisher(string selectedPublisher = null)
+        {
+            //publishers is used to hold all of the information on the PUBLISHERs table from the book database
+            var publishers = database.PUBLISHERs.ToList();
+            //Publishers is used to hold the selected publisher code and name of the selectedPublisher
+            ViewBag.Publishers = new SelectList(publishers, "PUBLISHER_CODE", "PUBLISHER_NAME", selectedPublisher); 
+            //books is used to hold all of the information on the selectedPublisher
+            var books = string.IsNullOrEmpty(selectedPublisher)
+                        ? new List<BOOK>()  
+                        : database.BOOKs.Where(b => b.PUBLISHER_CODE == selectedPublisher).ToList();
+            //Books is used to hold the values of all the information stored within the books database
+            ViewBag.Books = books;
+            //return View is used to return the books database on the Publisher webpage
+            return View(books);
+        }
+        //BookDetials is used to view the information that is in a book
+        public ActionResult BookDetials()
+        {
+            return View();
+        }
+        //Contact is webpage used to connact manage by sumbiting an AJAX enabled form
+        public ActionResult Contact()
+        {
+            
+            return View();
+        }
+
+      
+    }
+}
---
+++ FlowerShop/Controllers/HomeController.cs
@@ -0,0 +1,165 @@
+﻿using FlowerShop.Models.EntityFramework;
+using FlowerShop.Models;
+using System.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlowerShop.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        //database is used to hold the Flower shop database
+        private FLOWERSHOPEntitiess database = new FLOWERSHOPEntitiess();
+        
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Home Page";
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "New User Registration";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact";
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            ViewBag.Message = "About Us";
+            return View();
+        }
+        public ActionResult Legal()
+        {
+            ViewBag.Message = "Legal";
+            return View();
+        }
+        public ActionResult ProjectOne(string searchTerm, string color, string size, string fromPrice,string toPrice, string buttonType)
+        {
+            //both boolian values isfilterVaild and issearchValid is used to check if the each section of the filters both search and filters are valid
+            bool isfilterValid = true;
+            bool issearchValid = true;
+            //filterError and searchError are used to hold the error display values to be displayed in a viewbag message
+            string filterError = null;
+            string searchError = null;
+            //theses are the viewbag messages that will be displaying the error within the partical view
+            ViewBag.FilterError = filterError;
+            ViewBag.SearchError = searchError;
+            //if statement is used to check if the filter button has been clicked on
+            if (buttonType == "Filter")
+            {
+                //the rest of these if statements are used to check if the values are null, empty or still at there defualt value
+                if (string.IsNullOrEmpty(color) || color == "Please Select a Color")
+                {
+                    if (string.IsNullOrEmpty(size) || size == "Please Select a Size")
+                    {
+                        if (string.IsNullOrEmpty(fromPrice) || fromPrice == "From:" || string.IsNullOrEmpty(toPrice) || toPrice == "To:")
+                        {
+                            
+                                filterError = "Please Check Your Filter Criteria";
+                                isfilterValid = false;
+                            
+                        }
+                    }
+                }
+
+                //the rest of these if statements are used to check if the values are null, empty or still at there defualt value
+                if (!string.IsNullOrEmpty(fromPrice) && !string.IsNullOrEmpty(toPrice) && fromPrice != "From:" && toPrice != "To:")
+                {
+                    //fPrice is used to hold the fromPrice value
+                    int fPrice = int.Parse(fromPrice);
+                    //tPrice is used to hold the toPrice value
+                    int tPrice = int.Parse(toPrice);
+                    //if statement is used to see if the forPrice is greater than toPrice
+                    if (fPrice > tPrice)
+                    {
+                        filterError = "Please Check Your Price Range";
+                        isfilterValid = false;
+                    }
+                }
+            }
+            //if statement is used to check if the search button has been clicked on
+            if (buttonType == "Search")
+            {
+                //the rest of these if statements are used to check if the values are null or empty
+                if (string.IsNullOrWhiteSpace(searchTerm))
+                {
+                    searchError = "Please Enter a Search Term";
+                    issearchValid = false;
+                }
+                
+            }
+            //if statement checks if the Filter area is not valid
+            if (!isfilterValid)
+            {
+                ViewBag.FilterError = filterError;
+
+            }
+            //if statement checks if the Search area is not vaild
+            if (!issearchValid)
+            {
+                ViewBag.SearchError = searchError;
+            }
+
+
+            //namespaceConvert converts the users text input into the namespace input which is used in the flower table names
+            String namespaceConvert(String input)
+            {
+                //lowerCase turns the input into all lower case letters
+                String lowerCase = input.ToLower();
+                //nameSpace is used to get the lower case string and find spaces then replaces the spaces with underscores
+                String nameSpace = lowerCase.Replace(" ", "_");
+                return nameSpace;
+            }
+            //flowers is used to hold all of the information within the FLOWER database
+            var flowers = database.FLOWERs.AsQueryable();
+
+            //all if statements are used to check if the string for the search filters are not null or if the search filters do not equal there defualt text value
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                //converted is used get the namespace once the searchTerm has been converted through the namespaceConvert() method
+                String converted = namespaceConvert(searchTerm);
+                //this statement is used to find flowers namespace names which contain some of the same letter values as the converted namespace
+                flowers = flowers.Where(f => f.FLOWER_NAME.Contains(converted));
+               
+
+            }
+
+            
+            if (!string.IsNullOrEmpty(color) && color != "Please Select a Color")
+            {
+                //this statement is used to find color names within the FLOWER database which are equal to color
+                flowers = flowers.Where(f => f.COLOR.COLOR_NAME == color);
+            }
+
+            
+            if (!string.IsNullOrEmpty(size) && size != "Please Select a Size")
+            {
+                //this statement is used to find flower sizes within the FLOWER database which are equal to size
+                flowers = flowers.Where(f => f.FLOWER_SIZE == size);
+            }
+
+            if (!string.IsNullOrEmpty(toPrice) && !string.IsNullOrEmpty(fromPrice) && toPrice != "To:" && fromPrice != "From:")
+            {
+                //these two int statements are used to convert fromPrice and toPrice into integers
+                int fPrice = int.Parse(fromPrice);
+                int tPrice = int.Parse(toPrice);
+                //this statement gets the flower prices from the FLOWER database then gets flowers that are between the values fPrice and tPrice respectivily 
+                flowers = flowers.Where(f => f.FLOWER_PRICE >= fPrice && f.FLOWER_PRICE <= tPrice);
+            }
+            
+            return View(flowers.ToList());
+        }
+
+
+
+
+
+    }
+}
